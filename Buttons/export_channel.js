472ec4a76fb4141e8affc7fba77c6f0ce090b707
Music Maker <joshuanejdl@gmail.com>
2025-03-22 00:13:00 -0500

    feat: added export channel modal

---
+++ Buttons/export_channel.js
@@ -0,0 +1,28 @@
+const GetExportCache = require("../Utils/Caching/GetExportCache")
+
+const modal = {
+	title: 'Export Channel',
+	custom_id: 'export-channel',
+	components: [{
+		type: 1,
+		components: [{
+			type: 4,
+			custom_id: 'data',
+			label: 'Channel name or ID',
+			style: 1,
+			min_length: 1,
+			max_length: 100,
+			required: true
+		}]
+	}]
+}
+
+module.exports = {
+	customID: 'export-channel',
+	execute: async function(interaction, client, args) {
+		const exportOptions = await GetExportCache(client, interaction);
+		if (!exportOptions) return;
+
+		await interaction.showModal(modal);
+	}
+}

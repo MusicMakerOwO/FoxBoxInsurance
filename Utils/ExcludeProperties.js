3aab090240196ea8fc91449bdb1d396e682d000d
Music Maker <joshuanejdl@gmail.com>
2025-04-02 00:31:05 -0500

    refactor: Remove unused utility function

--- Utils/ExcludeProperties.js
+++
@@ -1,7 +0,0 @@
-module.exports = function ExcludeProperties (obj, ...properties) {
-	const newObj = {};
-	for (const key in obj) {
-		if (!properties.includes(key)) newObj[key] = obj[key];
-	}
-	return newObj;
-}

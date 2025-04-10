299e77e633dcc4dfdd30acae3e29b73baa396c82
Music Maker <joshuanejdl@gmail.com>
2025-04-10 13:51:19 -0500

    feat: Add TOS check and caching for messages

--- Events/Messages.js
+++ Events/Messages.js
@@ -1,9 +1,22 @@
+const Database = require('../Utils/Database');
 const SimplifyMessage = require('../Utils/Parsers/SimplifyMessage');
 const { DownloadQueue, ASSET_TYPE } = require('../Utils/Processing/Images');
+
+const accepted = new Map();
+function AcceptedTerms(guildID) {
+	if (accepted.has(guildID)) return accepted.get(guildID);
+	// check database
+	const terms = Database.prepare(`SELECT accepted_terms FROM Guilds WHERE id = ?`).pluck().get(guildID);
+	accepted.set(guildID, terms || false);
+	return terms || false;
+}
 
 module.exports = {
 	name: 'messageCreate',
 	execute: async function(client, message) {
+		const accepted = AcceptedTerms(message.guild.id);
+		if (!accepted) return; // server owner must accept TOS before collecting data
+
 		if (message.author.id === client.user.id) return; // self
 		
 		// Simplify the message to save on ram
@@ -19,3 +32,5 @@ module.exports = {
 		}
 	}
 }
+
+module.exports.AcceptedCache = accepted;

ab8054fe1549d46825b80db7b1bae5729f83dc6b
Music Maker <joshuanejdl@gmail.com>
2025-04-19 00:08:39 -0500

    fix: Switched from discord link to website

--- Commands/Invite.js
+++ Commands/Invite.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { COLOR } = require('../Utils/Constants');
 
-const invite = 'https://discord.com/oauth2/authorize?client_id=1065103018212732938&permissions=137439267848&integration_type=0&scope=bot';
-
 const embed = {
 	color: COLOR.PRIMARY,
 	description: `

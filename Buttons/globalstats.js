171b1e1305d5f1a50632f51d68ac734a5e318104
Music Maker <joshuanejdl@gmail.com>
2025-04-29 23:30:17 -0500

    feat: New statistics :D

--- Buttons/globalstats.js
+++ Buttons/globalstats.js
@@ -21,37 +21,118 @@ function CalcuateMessageStats() {
 		return lastOutput;
 	}
 
-	const analytics = Database.prepare(`
-WITH selected AS (
-	SELECT id, user_id, guild_id, channel_id, content, sticker_id
-	FROM Messages
-	ORDER BY id DESC
-	LIMIT ?
-)
-SELECT
-	COUNT(*) AS message_count,
-	COUNT(DISTINCT user_id) AS user_count,
-	COUNT(DISTINCT guild_id) AS guild_count,
-	COUNT(DISTINCT channel_id) AS channel_count,
-	COUNT(DISTINCT sticker_id) AS sticker_count,
+	const MessageStats = Database.prepare(`
+		WITH selected_messages AS (
+			SELECT id, user_id, guild_id, channel_id, content, sticker_id, length
+			FROM Messages
+			ORDER BY id DESC
+			LIMIT ${STAT_SIZE}
+		)
+		SELECT
+			COUNT(*) AS message_count,
+			COUNT(DISTINCT user_id) AS user_count,
+			COUNT(DISTINCT guild_id) AS guild_count,
+			COUNT(DISTINCT channel_id) AS channel_count,
+			COUNT(DISTINCT sticker_id) AS sticker_count,
 
-	ROUND(AVG(LENGTH(content)), 2) AS avg_length
-FROM selected
-	`).get(STAT_SIZE);
+			ROUND(AVG(length), 2) AS avg_length
+		FROM selected_messages
+	`).get();
+
+	const FileStats = Database.prepare(`
+		WITH selected_files AS (
+			SELECT Messages.id as message_id, Assets.size as size, Assets.asset_id as file_id
+			FROM Messages
+			LEFT OUTER JOIN Attachments ON Messages.id = Attachments.message_id
+			LEFT OUTER JOIN Assets ON Attachments.asset_id = Assets.asset_id
+			ORDER BY Messages.id DESC
+			LIMIT ${STAT_SIZE}
+		)
+		SELECT
+			COUNT(DISTINCT message_id) as message_count,
+			COUNT(DISTINCT file_id) as file_count,
+			MAX(size) as max_file,
+			MIN(size) as min_file
+		FROM selected_files
+		WHERE size IS NOT NULL
+	`).get();
+
+	const EmojiStats = Database.prepare(`
+		WITH selected_emojis AS (
+			SELECT
+				Messages.id as message_id,
+				user_id,
+				Emojis.id as emoji_id
+			FROM Messages
+			LEFT OUTER JOIN MessageEmojis ON MessageEmojis.message_id = Messages.id
+			LEFT OUTER JOIN Emojis ON MessageEmojis.emoji_id = Emojis.id
+			ORDER BY Messages.id DESC
+			LIMIT ${STAT_SIZE}
+		)
+		SELECT
+			COUNT(emoji_id) as emojis,
+			COUNT(DISTINCT message_id) as messages
+		FROM selected_emojis
+		WHERE emoji_id IS NOT NULL
+	`).get();
+
+	const topEmoji = Database.prepare(`
+		WITH selected_emojis AS (
+			SELECT
+				Messages.id AS message_id,
+				Messages.user_id,
+				Emojis.id AS emoji_id
+			FROM Messages
+			LEFT JOIN MessageEmojis ON MessageEmojis.message_id = Messages.id
+			LEFT JOIN Emojis ON MessageEmojis.emoji_id = Emojis.id
+			WHERE Emojis.id IS NOT NULL
+			ORDER BY Messages.id DESC
+			LIMIT ${STAT_SIZE}
+		),
+		emoji_counts AS (
+			SELECT
+				message_id,
+				user_id,
+				COUNT(emoji_id) AS emoji_count
+			FROM selected_emojis
+			GROUP BY message_id
+		)
+		SELECT MAX(emoji_count) AS 'max'
+		FROM emoji_counts
+		WHERE emoji_count IS NOT NULL
+	`).get();
 
 	const output = {
 		color: COLOR.PRIMARY,
-		title: 'Stats for nerds',
+		title: '📊 Stats for nerds',
 		description: `\`\`\`
-Last ${analytics.message_count} messages
-- Guilds: ${analytics.guild_count}
-- Channels: ${analytics.channel_count}
-- Users: ${analytics.user_count} (${(analytics.message_count / analytics.user_count).toFixed(2)} msg/user)
+Last ${MessageStats.message_count} messages
+- Guilds: ${MessageStats.guild_count}
+- Channels: ${MessageStats.channel_count}
+- Users: ${MessageStats.user_count} (${(MessageStats.message_count / MessageStats.user_count).toFixed(2)} msg/user)
 
-- Avg Length: ${analytics.avg_length} characters
+- Avg Length: ${MessageStats.avg_length} characters
+
+Emoji Stats (${EmojiStats.emojis} emojis) *
+- Max emojis: ${topEmoji.max} emojis
+- Avg emojis: ${(EmojiStats.emojis / EmojiStats.messages).toFixed(2)} emojis/msg
+
+Files Stats (${FileStats.file_count} files) **
+- Max size: ${(FileStats.max_file / 1024 / 1024).toFixed(2)} MB
+- Min size: ${(FileStats.min_file / 1024).toFixed(2)} KB
+- Avg files: ${(FileStats.file_count / FileStats.message_count).toFixed(2)} files/msg
 \`\`\`
+
+**Quick Facts** \`\`\`
+Only ${(MessageStats.sticker_count / MessageStats.message_count * 100).toFixed(2)}% of messages have a sticker
+Only ${(FileStats.message_count / MessageStats.message_count * 100).toFixed(2)}% of messages have a file
+The average user sent ${(MessageStats.message_count / MessageStats.user_count).toFixed(2)} messages
+\`\`\`
+-# \\* Only messages with emojis are counted
+-# \\*\\* Only messages with files are counted
+
 Last updated <t:${Math.floor(Date.now() / 1000)}:R>
-Next update in <t:${Math.floor((Date.now() + SECONDS.MINUTE * 1000 * 30) / 1000)}:R>`
+Next update <t:${Math.floor((Date.now() + SECONDS.MINUTE * 1000 * 30) / 1000)}:R>`
 	}
 
 	lastOutput = output;

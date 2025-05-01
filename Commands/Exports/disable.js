963cb91ea9dc474ad18bc5961b1f27bf45aa7aab
Music Maker <joshuanejdl@gmail.com>
2025-05-01 16:35:51 -0500

    fix: Better command name for user experience

--- Commands/Exports/disable.js
+++ Commands/Exports/disable.js
@@ -19,7 +19,7 @@ Only server admins are allowed to set this up`
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('disableexport')
+		.setName('disablechannel')
 		.setDescription('Disable exports in a designated channel')
 		.addChannelOption(x => x
 			.setName('channel')

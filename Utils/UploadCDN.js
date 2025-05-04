f9bbd50ac5fb327cd71f5f97b5d609b48bfb9a01
Music Maker <joshuanejdl@gmail.com>
2025-05-03 23:18:45 -0500

    fix: Clean input for https requests

--- Utils/UploadCDN.js
+++ Utils/UploadCDN.js
@@ -1,4 +1,12 @@
 const https = require('https');
+
+// remove all non-alphanumeric characters
+function CleanName(input) {
+	if (typeof input !== 'string') throw new TypeError(`Input must be a string : Received ${typeof input}`);
+	// remove all non-ASCII characters
+	return input.replace(/[^a-zA-Z0-9-_]/g, '');
+}
+
 module.exports = async function Upload(name, extension, data, downloadLimit = 0) {
 	// POST cdn.notfbi.dev/upload
 	return new Promise((resolve, reject) => {
@@ -10,8 +18,8 @@ module.exports = async function Upload(name, extension, data, downloadLimit = 0)
 			headers: {
 				'Content-Type': 'application/octet-stream',
 				'Content-Length': data.length,
-				'name': name,
-				'ext': extension,
+				'name': CleanName(name),
+				'ext': CleanName(extension),
 				'key': process.env.CDN_KEY,
 				'download-limit': downloadLimit || null, // 0 -> null -> no limit
 			}

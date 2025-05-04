036d98d1fca004589ede0a2db34697f148d85a49
Music Maker <joshuanejdl@gmail.com>
2025-05-03 23:37:57 -0500

    feat: Changlog command

---
+++ Commands/Changelog.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { COLOR } = require('../Utils/Constants');
+
+const embed = {
+	color: COLOR.PRIMARY,
+	title: 'Fox Box Insurance : v2.0.1',
+	description: `
+Last updated: \`2025 May 3rd\`
+
+\\- This command lol
+\\- Minor bug fixes
+\\- Edge case for default avatars
+\\- Made some commands publically visible`
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('changelog')
+		.setDescription('New here? Check out what has changed!'),
+	execute: async function(interaction, client) {
+		await interaction.reply({ embeds: [embed] });
+	}
+}

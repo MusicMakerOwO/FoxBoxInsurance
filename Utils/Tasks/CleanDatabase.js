bca44d39432f5d7c0252f160b92862914a123653
Music Maker <joshuanejdl@gmail.com>
2025-05-08 16:57:31 -0500

    perf: Tweaked CleanDatabase task to reuse CTEs and conditionally VACUUM only if needed

--- Utils/Tasks/CleanDatabase.js
+++ Utils/Tasks/CleanDatabase.js
@@ -1,66 +1,62 @@
 const Database = require("../Database");
-const { success } = require("../Logs");
+const Log = require("../Logs");
 
-// Tables to check:
-// - Emojis
-// - Stickers
-// - Attachments
-// - Embeds
-// - EmbedFields
-// - MessageEmojis
 module.exports = async function CleanDatabase() {
 
 	const start = process.hrtime.bigint();
+
+	let didDelete = 0;
 	
 	Database.exec('BEGIN TRANSACTION');
 
-	Database.prepare(`
-		DELETE FROM MessageEmojis
-		WHERE message_id NOT IN (
-			SELECT id FROM Messages
-		)
-	`).run();
+	Database.exec(`
+		CREATE TEMP TABLE IF NOT EXISTS temp_messages AS SELECT id, sticker_id FROM Messages;
+		CREATE TEMP TABLE IF NOT EXISTS temp_embeds AS SELECT DISTINCT id FROM Embeds;
+	`);
 
-	Database.prepare(`
+	didDelete |= Database.prepare(`
+		DELETE FROM MessageEmojis
+		WHERE message_id NOT IN ( SELECT id FROM temp_messages )
+	`).run().changes;
+
+	// delete emojis that are not used in any message
+	didDelete |= Database.prepare(`
 		DELETE FROM Emojis
 		WHERE id NOT IN (
-			SELECT emoji_id FROM MessageEmojis
+			SELECT DISTINCT emoji_id FROM MessageEmojis
 		)
-	`).run();
+	`).run().changes;
 
-	Database.prepare(`
+	didDelete |= Database.prepare(`
 		DELETE FROM Stickers
-		WHERE id NOT IN (
-			SELECT sticker_id FROM Messages
-		)
-	`).run();
+		WHERE id NOT IN ( SELECT sticker_id FROM temp_messages )
+	`).run().changes;
 
-	Database.prepare(`
+	didDelete |= Database.prepare(`
 		DELETE FROM Attachments
-		WHERE message_id NOT IN (
-			SELECT id FROM Attachments
-		)
-	`).run();
+		WHERE message_id NOT IN ( SELECT id FROM temp_messages )
+	`).run().changes;
 
-	Database.prepare(`
+	didDelete |= Database.prepare(`
 		DELETE FROM Embeds
-		WHERE message_id NOT IN (
-			SELECT id FROM Messages
-		)
-	`).run();
+		WHERE message_id NOT IN ( SELECT id FROM temp_messages )
+	`).run().changes;
 
-	Database.prepare(`
+	didDelete |= Database.prepare(`
 		DELETE FROM EmbedFields
-		WHERE embed_id NOT IN (
-			SELECT id FROM Embeds
-		)
-	`).run();
+		WHERE embed_id NOT IN ( SELECT DISTINCT id FROM temp_embeds)
+	`).run().changes;
+
+	Database.exec(`
+		DROP TABLE IF EXISTS temp_messages;
+		DROP TABLE IF EXISTS temp_embeds;
+	`);
 
 	Database.exec('COMMIT TRANSACTION');
 
-	Database.exec('VACUUM');
+	if (didDelete > 0) Database.exec('VACUUM');
 
 	const end = process.hrtime.bigint();
 	const duration = Number(end - start) / 1e6;
-	success(`Cleaned database in ${duration.toFixed(2)} ms`);
+	Log.success(`Cleaned database in ${duration.toFixed(2)} ms`);
 }

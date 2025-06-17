08a0d94fec8453df0cb90c15c5e085587b2af7a7
Music Maker <joshuanejdl@gmail.com>
2025-06-17 14:05:32 -0500

    feat: Import a snapshot :>

---
+++ Buttons/Imports/confirm.js
@@ -0,0 +1,44 @@
+const { SECONDS, EMOJI } = require("../../Utils/Constants");
+
+module.exports = {
+	customID: 'import-confirm',
+	execute: async function(interaction, client, args) {
+		const importID = args[0];
+
+		await interaction.deferUpdate({ ephemeral: true }).catch(() => { });
+
+		if (!client.ttlcache.has(`guild-imports-${interaction.guild.id}`)) {
+			client.ttlcache.set(`guild-imports-${interaction.guild.id}`, new Map([
+				[importID, Date.now() + SECONDS.MINUTE * 60 * 1000 ] // importID -> expiration timestamp
+			]), SECONDS.MINUTE * 60 * 1000); // Store the imports for 60 minutes
+			console.log(client.ttlcache.get(`guild-imports-${interaction.guild.id}`));
+		} else {
+			const imports = client.ttlcache.get(`guild-imports-${interaction.guild.id}`);
+			imports.set(importID, Date.now() + SECONDS.MINUTE * 60 * 1000);
+		}
+
+		const embed = {
+			color: 0x00FF00,
+			title: 'Snapshot Imported',
+			description: `
+The snapshot has been added your list, check it out with \`/snapshot list\`
+It will be removed from your list <t:${~~((Date.now() + SECONDS.MINUTE * 60 * 1000) / 1000)}:R>`
+		}
+
+		const gotoButton = {
+			type: 1,
+			components: [{
+				type: 2,
+				style: 2,
+				label: 'View Snapshots',
+				emoji: EMOJI.OPEN,
+				custom_id: 'snapshot-list'
+			}]
+		}
+
+		return interaction.editReply({
+			embeds: [embed],
+			components: [gotoButton]
+		});
+	}
+}

c7f137630095c543b3ca207cc345a42f44f4b62d
Music Maker <joshuanejdl@gmail.com>
2025-08-23 11:01:21 -0500

    Converted Utils/UserCanExport

--- Utils/UserCanExport.js
+++ Utils/UserCanExport.js
@@ -1,21 +1,19 @@
 const { GuildMember } = require("discord.js");
 const Database = require("./Database");
 
-module.exports = function CanUserExportChannel(member, channelID) {
+module.exports = async function CanUserExportChannel(member, channelID) {
 	if (!(member instanceof GuildMember)) throw new Error('First argument must be a GuildMember');
 	if (typeof channelID !== 'string') throw new TypeError('Second argument must be a string');
 
 	// admins bypass everything lol
 	const isAdmin = member.permissions.has('Administrator');
 	if (isAdmin) return true;
-	
-	// admin disabled the specific channel
-	const isDisabled = Database.prepare("SELECT block_exports FROM Channels WHERE id = ?").pluck().get(channelID);
-	if (isDisabled) return false;
-	
-	// banned users are blocked from using exports
-	const isBlocked = Database.prepare("SELECT user_id FROM GuildBlocks WHERE guild_id = ? AND user_id = ?").pluck().get(member.guild.id, channelID);
-	if (isBlocked) return false;
 
-	return true;
+	// admin disabled the specific channel
+	const [{ block_exports: isDisabled }] = await Database.query("SELECT block_exports FROM Channels WHERE id = ?", [channelID]);
+	if (isDisabled) return false;
+
+	// banned users are blocked from using exports
+	const [isBlocked] = await Database.query("SELECT user_id FROM GuildBlocks WHERE guild_id = ? AND user_id = ?", [member.guild.id, channelID]);
+	return isBlocked === undefined;
 }

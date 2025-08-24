4ffe5c5561cee1db8c1030143e7d1416e1038bda
Music Maker <joshuanejdl@gmail.com>
2025-08-24 00:16:28 -0500

    Converted Utils/Processing/Messages

--- Utils/Processing/Messages.js
+++ Utils/Processing/Messages.js
@@ -2,72 +2,14 @@ const CachePool = require('../Caching/CachePool');
 const Database = require('../Database');
 const sampleCache = new CachePool(3);
 
-const { DownloadQueue, ASSET_TYPE } = require('./Images');
+// const { DownloadQueue, ASSET_TYPE } = require('./Images');
 
 const Log = require('../Logs');
 const LinkAssets = require('./LinkAssets');
 const { FAILED_MESSAGES } = require('../Constants');
 const { writeFileSync } = require('node:fs');
 
-const MAX_CHUNK_SIZE = 1000;
-
-const FAILED_DATA = {}; // table -> data[]
-for (const table of Database.tables) {
-	FAILED_DATA[table] = [];
-}
-
-function BatchInsert(table, columns, conflict, data = []) {
-	if (!Database.tables.has(table)) {
-		// This is a bug in the code, not a user error
-		throw `Table ${table} does not exist in the database!`;
-	}
-
-	if (data.length === 0) return 0;
-
-	const placeholders = '(' + '?,'.repeat(columns.length - 1) + '?)'; // (?, ?, ?)
-
-	const maxBatchSize = Math.floor(MAX_CHUNK_SIZE / columns.length);
-	const batches = Math.ceil(data.length / maxBatchSize);
-
-	const queryStart = `INSERT INTO ${table} (${columns.join(',')}) VALUES `;
-
-	let queryCount = 0;
-
-	for (let i = 0; i < batches; i++) {
-		const start = i * maxBatchSize;
-		const end = Math.min(data.length, (i + 1) * maxBatchSize);
-
-		const chunk = data.slice(start, end)
-
-		const insert = queryStart + Array(chunk.length).fill(placeholders).join(', ') + ' ' + conflict;
-		const params = chunk.flat().map(x => typeof x === 'boolean' ? +x : x ?? null);
-
-		try {
-			Database.prepare(insert, true).run(params);
-			queryCount++;
-		} catch (error) {
-			queryCount += chunk.length;
-
-			// insert data one by one
-			const singleInsert = Database.prepare(`${queryStart} ${placeholders} ${conflict}`);
-			for (let j = 0; j < chunk.length; j++) {
-				const params = chunk[j].map(x => typeof x === 'boolean' ? +x : x ?? null);
-				try {
-					singleInsert.run(params);
-				} catch (error) {
-					Log.error(error);
-					FAILED_DATA[table].push({ data: params, error: error });
-				}
-			}
-		}
-	}
-
-	return queryCount;
-}
-
-const LastEmbedID = Database.prepare(`SELECT MAX(id) + 1 FROM embeds`);
-
-module.exports = function ProcessMessages (messageCache = sampleCache) {
+module.exports = async function ProcessMessages (messageCache = sampleCache) {
 	const messages = messageCache.cache[ messageCache.currentPool ];
 	if (messages.length === 0) return;
 
@@ -75,12 +17,9 @@ module.exports = function ProcessMessages (messageCache = sampleCache) {
 	// clear new pool
 	messageCache.clear();
 
-	let currentEmbedID = LastEmbedID.pluck().get(); // number
+	const connection = await Database.getConnection();
 
-	// clear failed data
-	for (const table in FAILED_DATA) {
-		if (FAILED_DATA[table].length > 0) FAILED_DATA[table] = [];
-	}
+	let [{ id: currentEmbedID }] = await connection.query(`SELECT IFNULL(MAX(id), 0) + 1 AS id FROM Embeds`);
 
 	Log.debug(`Processing ${messages.length} messages`);
 
@@ -108,7 +47,7 @@ module.exports = function ProcessMessages (messageCache = sampleCache) {
 		if (!Guilds.has(guild.id)	 ) Guilds.set(guild.id, guild);
 		if (!Channels.has(channel.id)) Channels.set(channel.id, channel);
 		if (!Users.has(user.id)		 ) Users.set(user.id, user);
-		
+
 		if (sticker && !Stickers.has(sticker.id)) Stickers.set(sticker.id, sticker);
 
 		if (message.emojis.length > 0) {
@@ -163,113 +102,105 @@ module.exports = function ProcessMessages (messageCache = sampleCache) {
 
 	const dedupeTime = process.hrtime.bigint();
 
-
 	// 2. Insert deduplicated data into database
 
-	Database.exec('BEGIN DEFERRED TRANSACTION');
-
-	let queryCount = 0;
+	await connection.beginTransaction();
 
 	try {
 
-		console.time('Guilds');
-		queryCount += BatchInsert(
-			'Guilds',
-			[ 'id', 'name' ],
-			'ON CONFLICT(id) DO UPDATE SET name=excluded.name WHERE excluded.name <> name',
-			GuildList.map(g => [g.id, g.name])
-		);
-		console.timeEnd('Guilds');
+		const insertBatches = [
+			connection.batch(`
+				INSERT INTO Guilds (id, name)
+				VALUES (?, ?)
+				ON DUPLICATE KEY UPDATE name = VALUES(name)
+			`, GuildList.map(g => [g.id, g.name])),
 
-		console.time('Channels');
-		queryCount += BatchInsert(
-			'Channels',
-			[ 'id', 'guild_id', 'parent_id', 'name', 'type' ],
-			'ON CONFLICT(id) DO UPDATE SET name=excluded.name WHERE excluded.name <> name',
-			ChannelList.map(c => [c.id, c.guildID, c.parentID, c.name, c.type])
-		);
-		console.timeEnd('Channels');
+			connection.batch(`
+				INSERT INTO Channels (id, guild_id, parent_id, name, type)
+				VALUES (?, ?, ?, ?, ?)
+				ON DUPLICATE KEY UPDATE name = VALUES(name)
+			`, ChannelList.map(c => [c.id, c.guildID, c.parentID, c.name, c.type])),
 
-		console.time('Users');
-		queryCount += BatchInsert(
-			'Users',
-			[ 'id', 'username', 'bot' ],
-			'ON CONFLICT(id) DO UPDATE SET username=excluded.username WHERE excluded.username <> username',
-			UserList.map(u => [u.id, u.username, u.bot])
-		);
-		console.timeEnd('Users');
+			connection.batch(`
+				INSERT INTO Users (id, username, bot)
+				VALUES (?, ?, ?)
+				ON DUPLICATE KEY UPDATE username = VALUES(username)
+			`, UserList.map(u => [u.id, u.username, +u.bot])),
+			connection.batch(`
+				INSERT INTO Messages (id, guild_id, channel_id, user_id, content, sticker_id, reply_to, length)
+				VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+			`, messages.map(m => [m.id, m.guild.id, m.channel.id, m.user.id, m.content, m.sticker?.id, m.reply_to, m.content?.length]))
+		];
 
-		console.time('Messages');
-		queryCount += BatchInsert(
-			'Messages',
-			[ 'id', 'guild_id', 'channel_id', 'user_id', 'content', 'sticker_id', 'reply_to', 'length' ],
-			'',
-			messages.map(m => [m.id, m.guild.id, m.channel.id, m.user.id, m.content, m.sticker?.id, m.reply_to, m.content?.length])
-		);
-		console.timeEnd('Messages');
+		if (EmojiList.length > 0) insertBatches.push(
+			connection.batch(`
+				INSERT INTO Emojis (id, name, animated)
+				VALUES (?, ?, ?)
+				ON DUPLICATE KEY UPDATE name = VALUES(name)
+			`, EmojiList.map(e => [e.id, e.name, +e.animated])),
 
-		console.time('Emojis');
-		queryCount += BatchInsert(
-			'Emojis',
-			[ 'id', 'name', 'animated' ],
-			'ON CONFLICT(id) DO UPDATE SET name=excluded.name WHERE excluded.name <> name',
-			EmojiList.map(e => [e.id, e.name, e.animated])
+			connection.batch(`
+				INSERT INTO MessageEmojis (message_id, emoji_id, count)
+				VALUES (?, ?, ?)
+			`, MessageEmojis.map(m => [m.messageID, m.emojiID, m.count]))
 		);
-		console.timeEnd('Emojis');
 
-		console.time('Stickers');
-		queryCount += BatchInsert(
-			'Stickers',
-			[ 'id', 'name' ],
-			'ON CONFLICT(id) DO UPDATE SET name=excluded.name WHERE excluded.name <> name',
-			StickerList.map(s => [s.id, s.name])
+		if (StickerList.length > 0) insertBatches.push(
+			connection.batch(`
+				INSERT INTO Stickers (id, name)
+				VALUES (?, ?)
+				ON DUPLICATE KEY UPDATE name = VALUES(name)
+			`, StickerList.map(s => [s.id, s.name]))
 		);
-		console.timeEnd('Stickers');
 
-		console.time('Attachments');
-		queryCount += BatchInsert(
-			'Attachments',
-			[ 'id', 'message_id', 'name' ],
-			'ON CONFLICT(id) DO UPDATE SET name=excluded.name WHERE excluded.name <> name',
-			AttachmentList.map(a => [a.id, a.messageID, a.name])
+		if (AttachmentList.length > 0) insertBatches.push(
+			connection.batch(`
+				INSERT INTO Attachments (id, message_id, name)
+				VALUES (?, ?, ?)
+				ON DUPLICATE KEY UPDATE name = VALUES(name)
+			`, AttachmentList.map(a => [a.id, a.messageID, a.name]))
 		);
-		console.timeEnd('Attachments');
 
-		console.time('Embeds');
-		queryCount += BatchInsert(
-			'Embeds',
-			[ 'id', 'message_id', 'title', 'description', 'url', 'timestamp', 'color', 'footer_text', 'footer_icon', 'thumbnail_url', 'image_url', 'author_name', 'author_url', 'author_icon' ],
-			'',
-			EmbedList.map(e => [e.id, e.messageID, e.title, e.description, e.url, e.timestamp, e.color, e.footer_text, e.footer_icon, e.thumbnail_url, e.image_url, e.author_name, e.author_url, e.author_icon])
+		if (EmbedList.length > 0) insertBatches.push(
+			connection.batch(`
+				INSERT INTO Embeds (
+					id, message_id,
+					title, description, url, timestamp, color,
+					footer_text, footer_icon,
+					thumbnail_url, image_url,
+					author_name, author_url, author_icon
+				)
+				VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+			`, EmbedList.map(e => [
+				e.id, e.messageID,
+				e.title, e.description, e.url, e.timestamp, e.color,
+				e.footer_text, e.footer_icon,
+				e.thumbnail_url, e.image_url,
+				e.author_name, e.author_url, e.author_icon
+			]))
 		);
-		console.timeEnd('Embeds');
 
-		console.time('EmbedFields');
-		queryCount += BatchInsert(
-			'EmbedFields',
-			[ 'embed_id', 'name', 'value', 'inline' ],
-			'',
-			FieldList.map(f => [f.embedID, f.name, f.value, f.inline])
+		if (FieldList.length > 0) insertBatches.push(
+			connection.batch(`
+				INSERT INTO EmbedFields (embed_id, name, value, inline)
+				VALUES (?, ?, ?, ?)
+			`, FieldList.map(f => [f.embedID, f.name, f.value, +f.inline]))
 		);
-		console.timeEnd('EmbedFields');
 
-		console.time('MessageEmojis');
-		queryCount += BatchInsert(
-			'MessageEmojis',
-			[ 'message_id', 'emoji_id', 'count' ],
-			'',
-			MessageEmojis.map(m => [m.messageID, m.emojiID, m.count])
-		);
-		console.timeEnd('MessageEmojis');
+		// wait for all writes to finish
+		// they will run independently, but we cannot commit until all are done
+		// the pain of race conditions in async code :(
+		await Promise.all(insertBatches);
 
-		console.time('Commit');
-		Database.exec('COMMIT TRANSACTION');
-		console.timeEnd('Commit');
+		connection.commit();
 	} catch (error) {
-		Database.exec('ROLLBACK TRANSACTION');
+		connection.rollback();
+		Database.releaseConnection(connection);
 		Log.error(error);
 		return; // do not print timing logs
 	}
+
+	Database.releaseConnection(connection);
 
 	const databaseTime = process.hrtime.bigint();
 
@@ -282,67 +213,54 @@ module.exports = function ProcessMessages (messageCache = sampleCache) {
 
 	Log.debug(`Deduplication took ${dedupeTimeMs.toFixed(3)}ms (${dedupeEffeciency.toFixed(3)}msg/sec)`);
 	Log.debug(`Database insertion took ${databaseTimeMs}ms (${databaseEffeciency.toFixed(3)}msg/sec)`);
-	Log.debug(`Executed ${queryCount} queries`);
 
-	// 3. Queue assets for download
-	for (const emoji of EmojiList) {
-		DownloadQueue.push({
-			type: ASSET_TYPE.EMOJI,
-			id: emoji.id,
-			name: emoji.name,
-			url: emoji.url, 
-			width: emoji.width,
-			height: emoji.height
-		});
-	}
-
-	for (const sticker of StickerList) {
-		DownloadQueue.push({
-			type: ASSET_TYPE.STICKER,
-			id: sticker.id,
-			name: sticker.name,
-			url: sticker.url, 
-			width: sticker.width,
-			height: sticker.height
-		});
-	}
-
-	for (const guild of GuildList) {
-		if (!guild.icon) continue;
-		DownloadQueue.push({
-			type: ASSET_TYPE.GUILD,
-			id: guild.id,
-			name: guild.name,
-			url: guild.icon.url,
-			width: guild.icon.width,
-			height: guild.icon.height
-		});
-	}
-
-	for (const user of UserList) {
-		if (!user.icon) continue;
-		DownloadQueue.push({
-			type: ASSET_TYPE.USER,
-			id: user.id,
-			name: user.username,
-			url: user.icon.url,
-			width: user.icon.width,
-			height: user.icon.height
-		});
-	}
-
+	// // 3. Queue assets for download
+	// for (const emoji of EmojiList) {
+	// 	DownloadQueue.push({
+	// 		type: ASSET_TYPE.EMOJI,
+	// 		id: emoji.id,
+	// 		name: emoji.name,
+	// 		url: emoji.url,
+	// 		width: emoji.width,
+	// 		height: emoji.height
+	// 	});
+	// }
+	//
+	// for (const sticker of StickerList) {
+	// 	DownloadQueue.push({
+	// 		type: ASSET_TYPE.STICKER,
+	// 		id: sticker.id,
+	// 		name: sticker.name,
+	// 		url: sticker.url,
+	// 		width: sticker.width,
+	// 		height: sticker.height
+	// 	});
+	// }
+	//
+	// for (const guild of GuildList) {
+	// 	if (!guild.icon) continue;
+	// 	DownloadQueue.push({
+	// 		type: ASSET_TYPE.GUILD,
+	// 		id: guild.id,
+	// 		name: guild.name,
+	// 		url: guild.icon.url,
+	// 		width: guild.icon.width,
+	// 		height: guild.icon.height
+	// 	});
+	// }
+	//
+	// for (const user of UserList) {
+	// 	if (!user.icon) continue;
+	// 	DownloadQueue.push({
+	// 		type: ASSET_TYPE.USER,
+	// 		id: user.id,
+	// 		name: user.username,
+	// 		url: user.icon.url,
+	// 		width: user.icon.width,
+	// 		height: user.icon.height
+	// 	});
+	// }
+	//
 	// 4. Link assets downloaded previously (not the ones in here)
 	LinkAssets();
-
-
-	// 5. Log failed data to disk
-	if (Object.values(FAILED_DATA).some(v => v.length > 0)) {
-		const failedData = Object.entries(FAILED_DATA).map(([table, data]) => {
-			return { table, data };
-		});
-		const failedDataString = JSON.stringify(failedData, null, 2);
-		const failedDataPath = `${FAILED_MESSAGES}/${Date.now()}.json`;
-		writeFileSync(failedDataPath, failedDataString);
-		Log.error(`Failed data written to ${failedDataPath}`);
-	}
 }

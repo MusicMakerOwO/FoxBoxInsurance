80ea846f00f6f4f7879a678b74d49da16da37d65
Music Maker <joshuanejdl@gmail.com>
2025-08-25 11:18:18 -0500

    Converted Utils/Tasks/EncryptMessages

--- Utils/Tasks/EncryptMessages.js
+++ Utils/Tasks/EncryptMessages.js
@@ -1,13 +1,10 @@
-const { promisify } = require("util")
 const Database = require("../Database")
 const Log = require("../Logs")
 const crypto = require("crypto")
-const ResolveUserKey = require("../ResolveUserKey")
-
-const scryptAsync = promisify(crypto.scrypt)
+const { ResolveUserKeyBulk } = require("../ResolveUserKey")
 
 module.exports = async function EncryptMessages() {
-	const unencryptedMessages = Database.prepare("SELECT id, user_id, content FROM Messages WHERE encrypted = 0").all();
+	const unencryptedMessages = await Database.query("SELECT id, user_id, content FROM Messages WHERE encrypted = 0");
 	if (unencryptedMessages.length === 0) {
 		Log.success("Nothing to encrypt");
 		return;
@@ -15,7 +12,11 @@ module.exports = async function EncryptMessages() {
 
 	Log.success(`Encrypting ${unencryptedMessages.length} messages...`);
 
-	const UpdateStatement = Database.prepare("UPDATE Messages SET content = ?, tag = ?, encrypted = 1 WHERE id = ?");
+	const connection = await Database.getConnection();
+
+	const UpdateStatement = await connection.prepare("UPDATE Messages SET content = ?, tag = ?, encrypted = 1 WHERE id = ?");
+
+	const userKeys = await ResolveUserKeyBulk(unencryptedMessages.map(m => m.user_id));
 
 	const start = process.hrtime.bigint();
 	for (const message of unencryptedMessages) {
@@ -24,7 +25,7 @@ module.exports = async function EncryptMessages() {
 			continue;
 		}
 
-		const key = ResolveUserKey(message.user_id);
+		const key = userKeys[message.user_id];
 
 		const iv = crypto.createHash("sha256").update(`${message.id}${message.user_id}`).digest("hex").slice(0, 16);
 		const cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
@@ -32,8 +33,11 @@ module.exports = async function EncryptMessages() {
 		const encrypted = cipher.update(message.content ?? '', "utf8", "base64") + cipher.final("base64");
 		const tag = cipher.getAuthTag().toString("base64");
 
-		UpdateStatement.run(encrypted, tag, message.id);
+		UpdateStatement.execute([encrypted, tag, message.id]);
 	}
+
+	UpdateStatement.close();
+	Database.releaseConnection(connection);
 
 	const end = process.hrtime.bigint();
 	const time = Number(end - start) / 1e6;

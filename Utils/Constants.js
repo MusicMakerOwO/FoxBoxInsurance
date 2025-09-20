6512c8cac20b7279c92e82269b2af7cfd29ff5df
Music Maker <joshuanejdl@gmail.com>
2025-09-19 23:11:13 -0500

    feat: Added WS op codes in constants

--- Utils/Constants.js
+++ Utils/Constants.js
@@ -128,6 +128,23 @@ const RESTORE_OPTION_NAMES = {
 	[RESTORE_OPTIONS.BANS]: '🚫 Bans'
 }
 
+const WebSocketOpCodes = {
+	// connection ops (100-199)
+	HEARTBEAT		: 100,
+	HEARTBEAT_ACK	: 101,
+	SERVER_ACK		: 102,
+	CLIENT_ACK		: 103,
+
+	// dispatch ops (200-299)
+	FLUSH_CACHE		: 200,
+
+	// errors (400-499)
+	JSON_PARSE_ERROR	: 400,
+	JSON_FORMAT_ERROR	: 401,
+	UNKNOWN_OP_CODE		: 402,
+	NO_RESPONSE			: 403,
+}
+
 module.exports = {
 	ROOT_FOLDER,
 
@@ -151,5 +168,7 @@ module.exports = {
 	RESTORE_OPTION_NAMES,
 
 	RandomLoadingMessage,
-	RandomLoadingEmbed
+	RandomLoadingEmbed,
+
+	WebSocketOpCodes
 }

9ae66e63af6e1c7c86829519012beb168ef40253
Music Maker <joshuanejdl@gmail.com>
2025-10-03 12:43:17 -0500

    refactor: Converted LinkAssets() to use a promise queue and properly await everything

--- Utils/Processing/LinkAssets.js
+++ Utils/Processing/LinkAssets.js
@@ -12,18 +12,22 @@ const LinkTables = [
 module.exports = async function LinkAssets () {
 	const connection = await Database.getConnection();
 	const start = process.hrtime.bigint();
+	const promiseQueue = [];
 	for (let i = 0; i < LinkTables.length; i++) {
 		const targetTable = LinkTables[i];
-		connection.query(`
-			UPDATE ${targetTable} as asset
-			SET asset_id = (
-				SELECT asset_id
-				FROM Assets
-				WHERE discord_id = asset.id
-			)
-			WHERE asset_id IS NULL	
-		`);
+		promiseQueue.push(
+			connection.query(`
+				UPDATE ${targetTable} as asset
+				SET asset_id = (
+					SELECT asset_id
+					FROM Assets
+					WHERE discord_id = asset.id
+				)
+				WHERE asset_id IS NULL	
+			`)
+		);
 	}
+	await Promise.all(promiseQueue);
 	const end = process.hrtime.bigint();
 	Database.releaseConnection(connection);
 	Log.debug(`Linked assets in ${(Number(end - start) / 1e6).toFixed(3)}ms`);

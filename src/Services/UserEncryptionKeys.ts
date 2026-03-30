938108d7cf12ca6ebfbf81e62e2ea95672b046d5
Music Maker <joshuanejdl@gmail.com>
2026-03-30 10:29:01 -0500

    refactor: Ported user keys to use the new encryption versioning system

--- src/Services/UserEncryptionKeys.ts
+++ src/Services/UserEncryptionKeys.ts
@@ -1,25 +1,27 @@
 import {randomBytes} from "node:crypto";
 import {SimpleUser} from "../Typings/DatabaseTypes";
 import {GetUser, SaveUser} from "../CRUD/Users";
-import {UnwrapUserKey, WrapUserKey} from "../Utils/Encryption/KeyWrapper";
+import * as v1 from "../Utils/Encryption/Versions/v1"
 
 function BuildNewKey() {
 	return randomBytes(32);
 }
 
+/** Automatically unwraps the key */
 export async function ResolveUserKey(userID: SimpleUser['id']) {
 	const savedUser = await GetUser(userID);
 	if (!savedUser) throw new Error('User not found');
-	if (savedUser.wrapped_key) return UnwrapUserKey(savedUser.wrapped_key);
+	if (savedUser.wrapped_key) return v1.Decrypt(savedUser.wrapped_key, Buffer.from(process.env.PEPPER!, 'base64'));
 
 	const newKey = BuildNewKey();
-	savedUser.wrapped_key = WrapUserKey(newKey);
+	savedUser.wrapped_key = v1.Encrypt(newKey, Buffer.from(process.env.PEPPER!, 'base64'));
 
 	void SaveUser(savedUser);
 
 	return newKey;
 }
 
+/** Returns a map of user IDs to unwrapped keys */
 export async function ResolveUserKeyBulk(userIDs: SimpleUser['id'][]) {
 	const result = new Map<SimpleUser['id'], Buffer>();
 

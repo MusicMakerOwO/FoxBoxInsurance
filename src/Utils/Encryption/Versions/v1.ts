edf43f5a5ed4b602cf0b45a8a4f862aebbde7095
Music Maker <joshuanejdl@gmail.com>
2026-03-31 10:31:14 -0500

    refactor: Renamed some variables

--- src/Utils/Encryption/Versions/v1.ts
+++ src/Utils/Encryption/Versions/v1.ts
@@ -1,19 +1,19 @@
 import {createCipheriv, createDecipheriv, randomBytes} from "node:crypto";
 
-export function Encrypt(content: Buffer, wrappingKey: Buffer) {
+export function Encrypt(content: Buffer, key: Buffer) {
 	const iv = randomBytes(12);
-	const cipher = createCipheriv('aes-256-gcm', wrappingKey, iv);
-	const wrapped = Buffer.concat([cipher.update(content), cipher.final()]);
+	const cipher = createCipheriv('aes-256-gcm', key, iv);
+	const cipherText = Buffer.concat([cipher.update(content), cipher.final()]);
 	const tag = cipher.getAuthTag();
-	return Buffer.concat([iv, wrapped, tag]); // 12 + length(wrapped) + 16
+	return Buffer.concat([iv, cipherText, tag]); // 12 + length(wrapped) + 16
 }
 
-export function Decrypt(wrappedBlob: Buffer, wrappingKey: Buffer) {
+export function Decrypt(wrappedBlob: Buffer, key: Buffer) {
 	const iv = wrappedBlob.subarray(0, 12);
 	const tag = wrappedBlob.subarray(wrappedBlob.length - 16);
 	// encrypted key is in the middle
 	const encryptedKey = wrappedBlob.subarray(12, wrappedBlob.length - 16);
-	const decipher = createDecipheriv('aes-256-gcm', wrappingKey, iv);
-	decipher.setAuthTag(tag);
-	return Buffer.concat([decipher.update(encryptedKey), decipher.final()]); // original key
+	const content = createDecipheriv('aes-256-gcm', key, iv);
+	content.setAuthTag(tag);
+	return Buffer.concat([content.update(encryptedKey), content.final()]); // original key
 }

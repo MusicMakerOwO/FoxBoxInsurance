33c6110370495d9e2cca1042e2f197447707be40
Music Maker <joshuanejdl@gmail.com>
2026-04-14 15:06:11 -0500

    chore: Made the embed text more condensed

--- src/Buttons/Snapshots/List.ts
+++ src/Buttons/Snapshots/List.ts
@@ -118,7 +118,7 @@ Create one using \`/snapshot create\``
 
 				const queuedDeletion = await isSnapshotQueuedForDeletion(item.id);
 
-				embed.description += `\n
+				embed.description += `
 ${queuedDeletion ? `**${EMOJI.WARNING} This snapshot is pending deletion**` : ''}
 ${emoji} **Snapshot #${item.id}** - \`${SNAPSHOT_TYPE_NAME[snapshotData.type]}\` ${SNAPSHOT_TYPE_EMOJI[snapshotData.type]}
 | Channels: ${snapshotData.channels.size}

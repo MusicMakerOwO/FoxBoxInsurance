5e8f3eb1c1315e9eb8b6a9bc5aa4272acc91a338
Music Maker <joshuanejdl@gmail.com>
2026-04-14 14:51:42 -0500

    fix: Updated global stats to the new database schemas

--- src/Buttons/GlobalStats.ts
+++ src/Buttons/GlobalStats.ts
@@ -2,11 +2,23 @@ import {ButtonHandler} from "../Typings/HandlerTypes";
 import {COLOR, SECONDS} from "../Utils/Constants";
 import {Database} from "../Database";
 import {APIEmbed} from "discord-api-types/v10";
+import { Asset, SimpleMessage } from "../Typings/DatabaseTypes";
 
 const STAT_SIZE = 10_000;
 
-function DiscordIDToDate(id = 0n) {
-	return new Date(Number((id >> 22n) + 1420070400000n));
+function FileSize(bytes: number): string {
+	if (bytes < 1024) return `${bytes} byte(s)`;
+
+	const units = ['KB', 'MB', 'GB', 'TB', 'TB'];
+	let size = bytes / 1024;
+	let unitIndex = 0;
+
+	while (size >= 1024 && unitIndex < units.length - 1) {
+		size /= 1024;
+		unitIndex++;
+	}
+
+	return `${size.toFixed(2)} ${units[unitIndex]}`;
 }
 
 let lastOutput = {};
@@ -19,143 +31,74 @@ async function CalcuateMessageStats(): Promise<APIEmbed> {
 
 	const connection = await Database.getConnection();
 
+	const selectedMessages = await connection.query(`
+		SELECT *
+		FROM Messages
+		ORDER BY id DESC
+		LIMIT ${STAT_SIZE}
+	`) as SimpleMessage[];
 
+	// messages are returned in reverse chronological order
+	const endDate = selectedMessages[0].created_at;
+	const startDate = selectedMessages[ selectedMessages.length - 1 ].created_at;
 
-	const MessageStats = await connection.query(`
-		WITH selected_messages AS (
-			SELECT id, user_id, guild_id, channel_id, content, sticker_id, length
-			FROM Messages
-			ORDER BY id DESC
-			LIMIT ${STAT_SIZE}
-		)
-		SELECT
-			COUNT(*) AS message_count,
-			COUNT(DISTINCT user_id) AS user_count,
-			COUNT(DISTINCT guild_id) AS guild_count,
-			COUNT(DISTINCT channel_id) AS channel_count,
-			COUNT(DISTINCT sticker_id) AS sticker_count,
+	const guildIDs = new Set<SimpleMessage['guild_id']>();
+	const channelIDs = new Set<SimpleMessage['channel_id']>();
+	const userIDs = new Set<SimpleMessage['user_id']>();
+	const stickerIDs = new Set<NonNullable<SimpleMessage['sticker_id']>>();
 
-			ROUND(AVG(length), 2) AS avg_length
-		FROM selected_messages
-	`).then( rows => rows[0] ) as {
-		message_count: bigint,
-		user_count: bigint,
-		guild_count: bigint,
-		channel_count: bigint,
-		sticker_count: bigint,
-		avg_length: number
-	};
+	for (const message of selectedMessages) {
+		guildIDs.add(message.guild_id);
+		channelIDs.add(message.channel_id);
+		userIDs.add(message.user_id);
+		if (message.sticker_id) stickerIDs.add(message.sticker_id);
+	}
 
-	// TODO: Attachments have been merged into Messages table, see SimpleMessage.data in Types/DatabaseTypes
-	const [FileStats] = await connection.query(`
-		WITH selected_files AS (
-			SELECT Messages.id as message_id, Assets.size as size, Assets.asset_id as file_id
-			FROM Messages
-			LEFT OUTER JOIN Attachments ON Messages.id = Attachments.message_id
-			LEFT OUTER JOIN Assets ON Attachments.asset_id = Assets.asset_id
-			ORDER BY Messages.id DESC
-			LIMIT ${STAT_SIZE}
-		)
-		SELECT
-			COUNT(DISTINCT message_id) as message_count,
-			COUNT(DISTINCT file_id) as file_count,
-			MAX(size) as max_file,
-			MIN(size) as min_file
-		FROM selected_files
-		WHERE size IS NOT NULL
-	`);
+	const avgLength = selectedMessages.reduce((acc, msg) => acc + (msg.length ?? 0), 0) / selectedMessages.length;
 
-	const [EmojiStats] = await connection.query(`
-		WITH selected_emojis AS (
-			SELECT
-				Messages.id as message_id,
-				user_id,
-				Emojis.id as emoji_id
-			FROM Messages
-			LEFT OUTER JOIN MessageEmojis ON MessageEmojis.message_id = Messages.id
-			LEFT OUTER JOIN Emojis ON MessageEmojis.emoji_id = Emojis.id
-			ORDER BY Messages.id DESC
-			LIMIT ${STAT_SIZE}
-		)
-		SELECT
-			COUNT(emoji_id) as emojis,
-			COUNT(DISTINCT message_id) as messages
-		FROM selected_emojis
-		WHERE emoji_id IS NOT NULL
-	`);
+	const messagesWithDiscordEmojis = selectedMessages.filter(msg => msg.data.emoji_ids.length > 0);
+	const avgEmojis = messagesWithDiscordEmojis.reduce((acc, msg) => acc + msg.data.emoji_ids.length, 0) / messagesWithDiscordEmojis.length;
+	const maxEmojis = messagesWithDiscordEmojis.reduce((max, msg) => Math.max(max, msg.data.emoji_ids.length), 0);
 
-	const [topEmoji] = await connection.query(`
-		WITH selected_emojis AS (
-			SELECT
-				Messages.id AS message_id,
-				Messages.user_id,
-				Emojis.id AS emoji_id
-			FROM Messages
-			LEFT JOIN MessageEmojis ON MessageEmojis.message_id = Messages.id
-			LEFT JOIN Emojis ON MessageEmojis.emoji_id = Emojis.id
-			WHERE Emojis.id IS NOT NULL
-			ORDER BY Messages.id DESC
-			LIMIT ${STAT_SIZE}
-		),
-		emoji_counts AS (
-			SELECT
-				message_id,
-				user_id,
-				COUNT(emoji_id) AS emoji_count
-			FROM selected_emojis
-			GROUP BY message_id
-		)
-		SELECT MAX(emoji_count) AS 'max'
-		FROM emoji_counts
-		WHERE emoji_count IS NOT NULL
-	`);
+	const messagesWithAttachments = selectedMessages.filter(msg => msg.data.attachments.length > 0);
+	const attachmentAssets = await connection.query(`
+		SELECT * FROM Assets WHERE discord_id IN (${new Array(messagesWithAttachments.length).fill('?').join(',')})
+	`, messagesWithAttachments.flatMap(msg => msg.data.attachments.map(x => x.id))) as Asset[];
 
-	const [timespan] = await connection.query(`
-		WITH selected_messages AS (
-			SELECT id, length
-			FROM Messages
-			ORDER BY id DESC
-			LIMIT ${~~(STAT_SIZE / 10)} -- 1/10 of the messages
-		)
-		SELECT
-			MAX(id) as max_id,
-			MIN(id) as min_id,
-			COUNT(*) as "count"
-		FROM selected_messages
-	`);
+	const totalFiles = messagesWithAttachments.reduce( (acc, msg) => acc + msg.data.attachments.length, 0);
+	const maxFileSize = attachmentAssets.reduce( (max, asset) => Math.max(max, asset.size), 0);
+	const minFileSize = attachmentAssets.reduce( (min, asset) => Math.min(min, asset.size), Infinity);
 
 	Database.releaseConnection(connection);
 
-	const maxDate = DiscordIDToDate(BigInt(timespan.max_id));
-	const minDate = DiscordIDToDate(BigInt(timespan.min_id));
-	const timeDiff = Math.abs(maxDate.getTime() - minDate.getTime());
-	const rate = Number(timespan.count) / (timeDiff / 1000 / 60); // messages per minute
+	const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+	const rate = STAT_SIZE / (timeDiff / 1000 / 60); // messages per minute
 
 	const output: APIEmbed = {
 		color: COLOR.PRIMARY,
 		title: '📊 Stats for nerds',
 		description: `\`\`\`
-Last ${MessageStats.message_count} messages
-- Guilds: ${MessageStats.guild_count}
-- Channels: ${MessageStats.channel_count}
-- Users: ${MessageStats.user_count} (${Number(MessageStats.message_count / MessageStats.user_count).toFixed(2)} msg/user)
+Last ${STAT_SIZE} messages
+- Guilds: ${guildIDs.size}
+- Channels: ${channelIDs.size}
+- Users: ${userIDs.size} (${(STAT_SIZE / userIDs.size).toFixed(2)} msg/user)
 
-- Avg Length: ${MessageStats.avg_length} characters
+- Avg Length: ${Math.round(avgLength)} characters
 
-Emoji Stats (${EmojiStats.emojis} emojis) *
-- Max emojis: ${topEmoji.max} emojis
-- Avg emojis: ${(Number(EmojiStats.emojis) / Number(EmojiStats.messages)).toFixed(2)} emojis/msg
+Emoji Stats (${messagesWithDiscordEmojis.length} emojis) *
+- Max emojis: ${maxEmojis} emojis
+- Avg emojis: ${avgEmojis.toFixed(2)} emojis/msg
 
-Files Stats (${FileStats.file_count} files) **
-- Max size: ${(Number(FileStats.max_file) / 1024 / 1024).toFixed(2)} MB
-- Min size: ${(Number(FileStats.min_file) / 1024).toFixed(2)} KB
-- Avg files: ${(Number(FileStats.file_count) / Number(FileStats.message_count)).toFixed(2)} files/msg
+Files Stats (${totalFiles} files) **
+- Max size: ${ FileSize(maxFileSize) }
+- Min size: ${ FileSize(minFileSize) }
+- Avg files: ${(totalFiles / messagesWithAttachments.length).toFixed(2)} files/msg
 \`\`\`
 
 **Quick Facts** \`\`\`
-Only ${(Number(MessageStats.sticker_count) / Number(MessageStats.message_count) * 100).toFixed(2)}% of messages have a sticker
-Only ${(Number(FileStats.message_count) / Number(MessageStats.message_count) * 100).toFixed(2)}% of messages have a file
-The average user sent ${(Number(MessageStats.message_count) / Number(MessageStats.user_count)).toFixed(2)} messages
+Only ${(stickerIDs.size / STAT_SIZE * 100).toFixed(2)}% of messages have a sticker
+Only ${(messagesWithAttachments.length / STAT_SIZE * 100).toFixed(2)}% of messages have a file
+The average user sent ${(STAT_SIZE / userIDs.size).toFixed(2)} messages
 On average, ${rate.toFixed(2)} messages are sent per minute
 \`\`\`
 -# \\* Only messages with emojis are counted
@@ -179,12 +122,10 @@ export default {
 	hidden        : false,
 	customID      : 'global-stats',
 	execute       : async function() {
-		const start = process.hrtime.bigint();
+		console.time('Calculating stats');
 		const stats = await CalcuateMessageStats();
-		const end = process.hrtime.bigint();
-		const elapsed = Number(end - start) / 1e6;
+		console.timeEnd('Calculating stats');
 
-		console.log(`Stats calculated in ${elapsed.toFixed(3)}ms`);
 		return {
 			embeds: [stats],
 			components: [{

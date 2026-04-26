864789890cf10d5e0b3d96e9c6d3ce24894aa2de
Music Maker <joshuanejdl@gmail.com>
2026-04-26 16:02:21 -0500

    fix: Message exports generated their hash via file name instead of file data

--- src/Utils/Parsers/Export.ts
+++ src/Utils/Parsers/Export.ts
@@ -240,7 +240,7 @@ export async function ExportChannel(options: ExportOptions) {
 	// memes_export.txt
 	const fileName = context.channel.name + '_export.' + FORMAT_NAMES[options.format].toLowerCase();
 	const hash = createHash('sha256')
-	.update(fileName)
+	.update(fileData)
 	.digest('hex');
 
 	Database.releaseConnection(connection);
